7063b41798ca41a05552b65cc66bd0bc74db3d9c
n4uf4l~17 <naufalsururi10@gmail.com>
2025-11-30 12:37:44 +0700

    Delete Pembelajaran Koding dan Kecerdasan Artificial/Mahir Programming Python/files.py

--- Pembelajaran Koding dan Kecerdasan Artificial/Mahir Programming Python/files.py
+++
@@ -1,6 +0,0 @@
-# (eps 15 : File Processing)
-
-file = open("coding-studio.txt", "a")
-# file.write("Ini adalah text yang baru")
-file.write("\nIni adalah text yang diappend dengan enter")
-file.close()
