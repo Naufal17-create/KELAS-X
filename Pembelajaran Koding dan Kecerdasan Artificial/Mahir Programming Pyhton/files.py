2232b4987fa31d4221dfe5986f28552362f68715
n4uf4l~17 <naufalsururi10@gmail.com>
2025-11-30 12:31:25 +0700

    Add files via upload

---
+++ Pembelajaran Koding dan Kecerdasan Artificial/Mahir Programming Pyhton/files.py
@@ -0,0 +1,6 @@
+# (eps 15 : File Processing)
+
+file = open("coding-studio.txt", "a")
+# file.write("Ini adalah text yang baru")
+file.write("\nIni adalah text yang diappend dengan enter")
+file.close()
