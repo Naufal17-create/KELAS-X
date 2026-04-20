d99a4cfb031a3d9348616358ffd7b76f5849660d
n4uf4l~17 <naufalsururi10@gmail.com>
2026-04-20 09:28:48 +0700

    Add files via upload

---
+++ X RPL/Kelas/20-April-2026(ConstructorOverloading)/src/ConstructorOverloading/Apsiswa.java
@@ -0,0 +1,15 @@
+package ConstructorOverloading;
+
+public class Apsiswa {
+
+    public static void main(String[] args) {
+        Siswa objek= new Siswa();
+            System.out.println("");
+        Siswa objek1= new Siswa("Bilal");
+            System.out.println("");
+        Siswa objek2= new Siswa("Adit",17);
+            System.out.println("");
+        Siswa objek3= new Siswa("Ragil",16,"Sioarjo");
+    }
+    
+}
