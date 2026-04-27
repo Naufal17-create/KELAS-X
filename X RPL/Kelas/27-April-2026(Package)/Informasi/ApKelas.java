4ccb93cdcef65148f7c03ad785ee6c303a051fe4
n4uf4l~17 <naufalsururi10@gmail.com>
2026-04-27 09:28:13 +0700

    Add files via upload

---
+++ X RPL/Kelas/27-April-2026(Package)/Informasi/ApKelas.java
@@ -0,0 +1,10 @@
+package Informasi;
+
+public class ApKelas {
+    public static void main(String[] args) {
+        Kelas obj = new Kelas();
+        System.out.println(obj.nim);
+        System.out.println(obj.nama);
+        System.out.println(obj.jurusan);
+    }
+}
---
+++ X RPL/Kelas/27-April-2026(Package)/Informasi/Kelas.java
@@ -0,0 +1,13 @@
+package Informasi;
+
+public class Kelas {
+   public String nim="20230001";
+   public String nama="Naufal"; 
+   public String jurusan="Rekayasa Perangkat Lunak";
+   
+   public void tampilkanInfo() {
+        System.out.println("NIM: " + nim);
+        System.out.println("Nama: " + nama);
+        System.out.println("Jurusan: " + jurusan);
+    }
+}
